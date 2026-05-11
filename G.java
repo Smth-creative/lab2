92d21802313454c3a3b8b99e47c83f392543905a
Blue <blue@example.com>
2026-05-11 10:57:13 +0300

    r1

--- G.java
+++ G.java
@@ -19,4 +19,8 @@ public class G implements J {
     public int hh() {
         return new java.util.Random(10).nextInt(10);
     }
+
+    public double ad() {
+        return java.lang.Math.sqrt(13);
+    }
 }
